4b96e9280264865894acf1acc3f29683bf3b9aa8
Arzu Hussein <arzu.huseyn@gmail.com>
2020-03-13 23:34:38 +0400

    lp3

--- sendmail.php
+++ sendmail.php
@@ -14,19 +14,18 @@ if($_POST["foo"]==''):/* if the input is not empty it means that a SPAM BOT fill
     $email = $_POST["email"];
     $phone = $_POST["phone"];
     $city = $_POST["city"];
-    $phone = $_POST["phone"];
 	
 	if($name=='' || $email=='' || $phone=='' || $zipcode==''): echo '2'; exit(); endif; /* if some values is missing, return the error */
 	
 	
 	$date=date("d M Y - h:1 a");
 
-	$mail='arzu.huseyn@gmail.com'; /* change this variable with your email address */
+	$mail='mike@allieddigitalmedia.com'; /* change this variable with your email address */
 	$header = "From: ".$name." <".$email.">" . "\r\n" . "Reply-To: ".$email."" . "\r\n" . "X-Mailer: PHP/" . phpversion();	
     $header .= "MIME-Version: 1.0\n";
 	$header .= "Content-Type: text/html; charset=\"iso-8859-1\"\n";
 	$header .= "Content-Transfer-Encoding: 7bit\n\n";
-	$msg= '<html><body>New message from landing Page.<br /><br /><strong>Name:</strong> '.$name.'<br /><br /><strong>Email:</strong> '.$email.'<br /><br /><strong>Message:</strong> '.nl2br($city).'<br /><br /><strong>Date:</strong> '.$date.'<br /><br /><br /><small><i>From - Your website</i></small></body></html>';
-	if(@mail("$mail","Your website - Contact form",$msg,$header)): echo '1'; else: echo '3'; endif;/* check if the mail() function succeded */
+	$msg= '<html><body>New message from http://homesolartoday.com/lp/.<br /><br /><strong>ZIP CODE:</strong> '.$zipcodepro.'<br /><br /><strong>Name:</strong> '.$name.'<br /><br /><strong>Surname:</strong> '.$lastname.'<br /><br /><strong>Email:</strong> '.$email.'<br /><br /><strong>How much sun exposure does your property get?:</strong> '.$sun.'<br /><br /><strong>Where would this project take place?:</strong> '.$projectplace.'<br /><br /><strong>What is your average monthly electricity bill?</strong> '.$bill.'<br /><br /><strong>Property Type:</strong> '.$property.'<br /><br /><strong>What kind of solar power solution are you looking for?</strong> '.$solarsolution.'<br /><br /><strong>Date:</strong> '.$date.'<br /><br /><br /><small><i>From - Your website</i></small></body></html>';
+	if(@mail("$mail","HOME SOLAR TODAY Campaign - Contact form",$msg,$header)): echo '1'; else: echo '3'; endif;/* check if the mail() function succeded */
 	
 else: echo '1'; endif;/* send a positive fake return if SPAM is detected */
